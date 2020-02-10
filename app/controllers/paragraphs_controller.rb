ce1983790df2dea7293bba34a9485cce4fdd0393
beekimrj <beekimrj@yahoo.com>
2020-02-10 11:35:21 +0545

    add create, read and edit options for paragraph and also select available hotels for paragraph

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -1,16 +1,60 @@
 class ParagraphsController < ApplicationController
+
+  def index
+    @paragraphs = Paragraph.all
+  end
+
+
+  def new
+    # @article = Article.find(params[:article_id])
+    # @paragraph =paragraph.create(params[:paragraph].permit(:title, :body))
+
+    # redirect_to article_path(@paragraph)
+    @paragraph = Paragraph.new
+  end
+
   def create
-    @article = Article.find(params[:article_id])
-    @paragraph = @article.paragraph.create(params[:paragraph].permit(:title, :body))
+    @paragraph = Paragraph.new(paragraph_params)
 
-    redirect_to article_path(@article)
+    if @paragraph.save
+        redirect_to '/paragraphs'
+    else
+      render 'new'
+    end
+  end
 
-end
+  def destroy
+      @article = Article.find(params[:article_id])
+      @paragraph = @article.paragraph.find(params[:id])
+      @paragraph.destroy
+      redirect_to article_path(@article)
+  end
 
-def destroy
-    @article = Article.find(params[:article_id])
-    @paragraph = @article.paragraph.find(params[:id])
-    @paragraph.destroy
-    redirect_to article_path(@article)
-end
+  def edit
+    @paragraph = Paragraph.find(params[:id])
+  end
+
+  def update
+    @paragraph = Paragraph.find(params[:id])
+    
+    # if(@hotel.check_in_date > @hotel.check_out_date)
+    #   # flash[:alert] = "You checked out in past!!"
+    #   render 'edit'
+    
+      if @paragraph.update(paragraph_params)
+      # flash[:notice] = "Updated successfully"
+      redirect_to paragraphs_path
+    else
+      render 'edit'
+    end
+
+  end
+
+
+
+
+  def paragraph_params
+    params.require(:paragraph).permit(:title, :body, :article_id, :hotel_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: "articles#index", as: 'home'
   resources :hotels
-  get 'paragraphs/create'
-  get 'paragraphs/destroy'
+  # get 'paragraphs/new'
+  # get 'paragraphs/destroy'
+  # get 'paragraphs/index'
+  # get 'paragraphs', to: "paragraphs#index"
+  resources :paragraphs
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
